ec9aa76b610f65e614765f7f42ef0e276e496246
mateus <mateusfelipern@gmail.com>
2022-05-07 18:23:03 -0300

    troca do desenho que a partícula faz, de pontos pra linhas.

--- perlim-noise-teste/sketch.js
+++ perlim-noise-teste/sketch.js
@@ -1,7 +1,6 @@
 var x = 0
 var y = 0
 var passo = 0.01
-
 var xoff1 = 0
 var xoff2 = 1000
 
@@ -11,6 +10,7 @@ class Particula {
   constructor(x, y, passo) {
     this.x = x
     this.y = y
+
     this.passo = passo
     this.historico = []
 
@@ -21,16 +21,21 @@ class Particula {
       xoff2 += this.passo
 
       this.vetor = createVector(this.x, this.y)
+
       this.historico.push(this.vetor)
+
     }
 
     this.show = function () {
       ellipse(this.x, this.y, 24, 24)
 
-      this.historico.forEach((element) => {
-        strokeWeight(2)
-        point(element.x, element.y)
-      })
+      if (this.historico.length > 1) {
+        for (let i = 1; i < this.historico.length; i++) {
+          let pAtual = this.historico[i]
+          let pAnterior = this.historico[i - 1]
+          line(pAtual.x, pAtual.y, pAnterior.x, pAnterior.y)
+        }
+      }
     }
   }
 }
@@ -44,4 +49,5 @@ function draw() {
   background(220)
   particula.update()
   particula.show()
+  // noLoop()
 }
